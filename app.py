fd5b54364e18486c9189e971a3a2c86e6bb051f6
Eliie58 <elies.y@hotmail.com>
2023-08-03 11:14:56 +0200

    First commit. Add streamlit app and weaviate connection.

---
+++ app.py
@@ -0,0 +1,101 @@
+# pylint: disable=broad-except
+'''
+Streamlit Web App
+'''
+import json
+from connection import WeaviateConnection
+import streamlit as st
+
+CONNECTION = st.experimental_connection(
+    'weaviate', type=WeaviateConnection)
+
+
+def validate_json(content: str) -> bool:
+    """
+    Validate json string.
+
+        Parameters
+    ----------
+    content : str
+
+    Returns
+    -------
+    bool
+        True if json is valid,
+        False otherwise.
+    """
+    try:
+        json.loads(content)
+        return True
+    except json.decoder.JSONDecodeError:
+        return False
+
+
+def layout() -> None:
+    """
+    Layout streamlit web app
+    """
+    st.title("Weaviate Streamlit Integration")
+    if CONNECTION.is_ready():
+        st.write("Connected to Weaviate")
+    else:
+        st.write("Not Connected to Weavite")
+
+    tab1, tab2, tab3, tab4 = st.tabs(
+        ["View Schema 🔭", "Create Schema 🏗️", "Create Data Object ✍", "Query Schema Objects 🧐"])
+
+    with tab1:
+        st.header("View Schema")
+        schema = CONNECTION.schema().get()
+        st.json(schema)
+
+    with tab2:
+        st.header("Create a new schema")
+        text = st.text_area("Schema Definition", height=200)
+        if st.button('Create schema'):
+            try:
+                CONNECTION.schema().create(json.loads(text))
+                st.toast("Schema created succesfully", icon="👏")
+            except json.decoder.JSONDecodeError:
+                st.toast("Invalid JSON in the schema", icon="🤦")
+            except Exception as ex:
+                st.toast("Failed creating schema", icon="🤦")
+
+    with tab3:
+        st.header("Add new Data Object")
+        classes = (cls["class"]
+                   for cls in CONNECTION.schema().get()["classes"])
+        class_name = st.selectbox(
+            "To Which class do you want to add this object?",
+            classes
+        )
+        text = st.text_area("Object Definition", height=200)
+        if st.button('Add Object'):
+            try:
+                if not class_name:
+                    st.toast("Please select Object class", icon="🤦")
+                if not text:
+                    st.toast("Please add Object Definition", icon="🤦")
+                uuid = CONNECTION.create(json.loads(text), class_name)
+                st.toast(f"Object added with UUID {uuid}", icon="👏")
+            except json.decoder.JSONDecodeError:
+                st.toast("Invalid JSON in the Object Definition", icon="🤦")
+            except Exception as ex:
+                print(ex)
+                st.toast("Failed adding Object", icon="🤦")
+
+    with tab4:
+        st.header("Query Data Objects")
+        classes = (cls["class"]
+                   for cls in CONNECTION.schema().get()["classes"])
+        class_name = st.selectbox(
+            "Which class do you want query?",
+            classes
+        )
+        if class_name:
+            all_objects = CONNECTION.data_object().get(class_name=class_name)
+            st.json(all_objects)
+
+
+if __name__ == "__main__":
+    layout()
---
+++ connection.py
@@ -0,0 +1,159 @@
+'''
+Weaviate connection with streamlit
+'''
+from typing import Dict
+
+import weaviate
+from weaviate.data import DataObject
+from weaviate.gql import Query
+from weaviate.schema import Schema
+from streamlit.connections import ExperimentalBaseConnection
+from streamlit.errors import StreamlitAPIException
+
+
+CONSISTENCY_LEVEL = weaviate.data.replication.ConsistencyLevel.ALL
+
+
+class WeaviateConnection(ExperimentalBaseConnection[weaviate.Client]):
+    '''
+    Weaviate Connection
+    '''
+
+    def _connect(self, **kwargs) -> weaviate.Client:
+        url = self._get_param('url')
+        if not url:
+            raise StreamlitAPIException(
+                "Missing Weaviate connection param: url")
+
+        # Get auth mode
+        # API
+        auth_type = self._get_param("auth_type")
+        if auth_type == 'API_KEY':
+            api_key = self._get_param("api_key")
+            if not api_key:
+                raise StreamlitAPIException(
+                    "Missing Weaviate connection param: api_key")
+            auth_config = weaviate.AuthApiKey(api_key=api_key)
+        elif auth_type == 'OIDC_OWNER':
+            username = self._get_param("username")
+            password = self._get_param("password")
+            scope = self._get_param("scope")
+            if not username:
+                raise StreamlitAPIException(
+                    "Missing Weaviate connection param: username")
+            if not password:
+                raise StreamlitAPIException(
+                    "Missing Weaviate connection param: password")
+            auth_config = weaviate.AuthClientPassword(
+                username=username,
+                password=password,
+                scope=scope
+            )
+        elif auth_type == 'OIDC_CLIENT':
+            client_secret = self._get_param("client_secret")
+            scope = self._get_param("scope")
+            if not client_secret:
+                raise StreamlitAPIException(
+                    "Missing Weaviate connection param: client_secret")
+            auth_config = weaviate.AuthClientCredentials(
+                client_secret=client_secret,
+                scope=scope
+            )
+        elif auth_type == 'OIDC_TOKEN':
+            access_token = self._get_param("access_token")
+            expires_in = self._get_param("expires_in")
+            refresh_token = self._get_param("refresh_token")
+            if not access_token:
+                raise StreamlitAPIException(
+                    "Missing Weaviate connection param: access_token")
+            if not expires_in:
+                expires_in = 60
+            auth_config = weaviate.AuthBearerToken(
+                access_token=access_token,
+                expires_in=expires_in,
+                refresh_token=refresh_token
+            )
+        else:
+            auth_config = None
+
+        if auth_config:
+            return weaviate.Client(url=url, auth_client_secret=auth_config, **kwargs)
+        return weaviate.Client(url=url, **kwargs)
+
+    def client(self) -> weaviate.Client:
+        '''
+        Get weaviate client
+        '''
+        return self._instance
+
+    def query(self) -> Query:
+        '''
+        Get Query object
+        '''
+        return self._instance.query
+
+    def is_ready(self):
+        """
+        Ping Weaviate's ready state
+
+        Returns
+        -------
+        bool
+            True if Weaviate is ready to accept requests,
+            False otherwise.
+        """
+        return self._instance.is_ready()
+
+    def schema(self) -> Schema:
+        """
+        Get Weaviate's schema
+
+        Returns
+        -------
+        Schema
+        """
+        return self._instance.schema
+
+    def data_object(self) -> DataObject:
+        """
+        Get Weaviate's DataObject
+
+        Returns
+        -------
+        DataObject
+        """
+        return self._instance.data_object
+
+    def create(self, data_obj: Dict, class_name: str) -> str:
+        """
+        Takes a dict describing the object and adds it to Weaviate.
+
+        Parameters
+        ----------
+        data_object : dict or str
+            Object to be added.
+            If type is str it should be either a URL or a file.
+        class_name : str
+            Class name associated with the object given.
+
+        Returns
+        -------
+        str
+            Returns the UUID of the created object if successful.
+
+        """
+        data_object = self._instance.data_object
+        return data_object.create(data_object=data_obj,
+                                  class_name=class_name,
+                                  consistency_level=CONSISTENCY_LEVEL
+                                  )
+
+    def _get_param(self, key: str, **kwargs) -> str:
+        if key in kwargs:
+            param = kwargs.pop(key)
+        else:
+            try:
+                param = self._secrets[key]
+            except KeyError:
+                param = None
+        return param
